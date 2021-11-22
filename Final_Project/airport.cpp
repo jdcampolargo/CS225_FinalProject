c9505165936d76e74f32abb4e126968360b41fa1
Campolargo Hoyos, Juan David <jcampo37@illinois.edu>
2021-11-22 11:11:50 -0600

    First Implementation of Dijkstra's algorithm

--- Final_Project/airport.cpp
+++ Final_Project/airport.cpp
@@ -8,6 +8,94 @@ Airport::Airport() {
 
 }
 
+
 Airport::~Airport() {
 
 }*/
+
+
+//Start of Dijkstra's algorithm
+
+import java.util.*;
+class Program {
+
+  // O(v^2 + e) time | O(v) space - where v is the number of
+  // vertices and e is the number of edges in the input graph
+  public int[] dijkstrasAlgorithm(int start, int[][][] edges) {
+
+    int numberOfVertices = edges.length;
+    int[] minDistances = new int[edges.length];
+    Arrays.fill(minDistances, Integer.MAX_VALUE);
+    minDistances[start] = 0;
+    Set<Integer> visited = new HashSet<Integer>();
+    while (visited.size() != numberOfVertices) {
+
+      int[] getVertexData = getVertexWithMinDistances(minDistances, visited);
+   int vertex = getVertexData[0];
+
+      int currentMinDistance = getVertexData[1];
+			
+		
+			if (currentMinDistance == Integer.MAX_VALUE) {
+				break;
+}
+	visited.add(vertex);
+			
+			for (int[] edge : edges[vertex]) {
+	
+				int destination = edge[0];
+				int distanceToDestination = edge[1];
+				
+				if (visited.contains(destination)) {
+					continue;
+        }		
+			}
+        int newPathDistance = currentMinDistance + distanceToDestination;
+
+        int currentDestinationDistance = minDistances[destination];
+
+        if (newPathDistance < currentDestinationDistance) {
+
+          minDistances[destination] = newPathDistance;
+
+        }
+    }
+    }
+    int[] finalDistances = new int[minDistances.length];
+    for (int i = 0; i < minDistances.length; i++) {
+
+      int distance = minDistances[i];
+
+      if (distance == Integer.MAX_VALUE) {
+        finalDistances[i] = -1;
+
+      } else {
+
+        finalDistances[i] = distance;
+
+      }
+
+    }
+
+    return finalDistances;
+
+  }
+  public int[] getVertexWithMinDistances(int[] distances, Set<Integer> visited) {
+int currentMinDistance = Integer.MAX_VALUE;
+    int vertex = -1;
+    for (int vertexIdx = 0; vertexIdx < distances.length; vertexIdx++) {
+      int distance = distances[vertexIdx];
+      if (visited.contains(vertexIdx)) {
+
+        continue;
+      }
+      if (distance <= currentMinDistance) {
+        vertex = vertexIdx;
+        currentMinDistance = distance;
+      }
+    }
+
+    return new int[] {vertex, currentMinDistance};
+
+  }
+}
